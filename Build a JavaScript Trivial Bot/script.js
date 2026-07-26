77099eb446fdc0748b3820ed9dcf05cddaf815b3
Raras Eka Pratiwi <142718916+raraseka@users.noreply.github.com>
2026-07-26 23:12:45 +0700

    Create script.js

---
+++ Build a JavaScript Trivial Bot/script.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun factguide!");
+let botName= "botName";
+let botLocation = "botLocation";
+let favoriteLanguage = "favoriteLanguage";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "codingFact";
+codingFact = "Your favourite language " + favoriteLanguage;
+
+console.log(codingFact);
+
+codingFact = "Do you have " + favoriteLanguage;
+console.log(codingFact);
+
+codingFact = "What's your " + favoriteLanguage;
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! " + "- " + botName + " from " + botLocation + ".");
+
+
