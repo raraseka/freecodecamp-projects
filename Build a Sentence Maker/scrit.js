4af9a89404fb117bdb1c2a94062eca0cc649a6ba
Raras Eka Pratiwi <142718916+raraseka@users.noreply.github.com>
2026-07-27 13:40:45 +0700

    Create scrit.js

---
+++ Build a Sentence Maker/scrit.js
@@ -0,0 +1,24 @@
+let adjective = "beautiful";
+let noun = "cat";
+let verb ="running";
+let place ="park";
+let adjective2 ="sunny";
+let noun2 ="tree";
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log(firstStory);
+
+console.log("First story: " + firstStory);
+
+let secondStory;
+adjective = "mysterious";
+noun = "dragon";
+verb = "sneezing";
+place = "castle";
+adjective2 = "gigantic";
+noun2 = "pancakes";
+
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log(secondStory);
+
+console.log("Second story: " + secondStory);
